489b03081adb0d91f836453ab038d969bde18338
Anderson Martins Silva <anderson_martins@sicredi.com.br>
2020-06-04 21:47:51 -0300

    Inclusao classe ClienteSaidaDto

---
+++ src/main/java/br/com/tt/petshop/dto/ClienteSaidaDto.java
@@ -0,0 +1,34 @@
+package br.com.tt.petshop.dto;
+
+import br.com.tt.petshop.model.Cliente;
+
+public class ClienteSaidaDto {
+    private Integer id;
+    private String nome;
+    private String cpf;
+
+    public ClienteSaidaDto(Cliente cliente){
+        this.id = cliente.getId();
+        this.nome = cliente.getNome();
+        this.cpf = cliente.getCpf();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getDescricao() {
+
+        return String.format("Nome: %s e Cpf: %s", this.nome, this.cpf);
+    }
+
+
+}
