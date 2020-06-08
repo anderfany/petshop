e742a24baf8f3803b44bf2a6af1e7ccec0bc0789
Anderson Martins Silva <anderson_martins@sicredi.com.br>
2020-06-08 19:53:33 -0300

    Inclusao classe MensagemErroDto

---
+++ src/main/java/br/com/tt/petshop/dto/MensagemErroDto.java
@@ -0,0 +1,21 @@
+package br.com.tt.petshop.dto;
+
+public class MensagemErroDto {
+    private String codigo;
+    private String mensagem;
+
+    public MensagemErroDto(String codigo, String mensagem) {
+        this.codigo = codigo;
+        this.mensagem = mensagem;
+    }
+
+    public String getCodigo() {
+
+        return codigo;
+    }
+
+    public String getMensagem() {
+
+        return mensagem;
+    }
+}
