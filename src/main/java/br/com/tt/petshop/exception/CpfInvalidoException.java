4162a8434de95f41a4f1c7629e57df947631eb2c
Anderson Martins Silva <anderson_martins@sicredi.com.br>
2020-06-08 21:10:19 -0300

    Removento http status code da exception pois o ExceptionHandler vai sobrescrever.

--- src/main/java/br/com/tt/petshop/exception/CpfInvalidoException.java
+++ src/main/java/br/com/tt/petshop/exception/CpfInvalidoException.java
@@ -3,7 +3,7 @@ package br.com.tt.petshop.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+//@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
 public class CpfInvalidoException extends RuntimeException {
 
     public CpfInvalidoException(String message) {
