7bbceb96ba84832aaea4c91f4048b1660661b18b
Anderson Martins Silva <anderson_martins@sicredi.com.br>
2020-06-11 20:45:06 -0300

    criacao classe

---
+++ src/main/java/br/com/tt/petshop/service/AnimalService.java
@@ -0,0 +1,23 @@
+package br.com.tt.petshop.service;
+
+import br.com.tt.petshop.model.Animal;
+import br.com.tt.petshop.repository.AnimalRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AnimalService {
+
+    //Atributo
+    private AnimalRepository animalRepository;
+
+    //Construtor
+    public AnimalService(AnimalRepository animalRepository) {
+        this.animalRepository = animalRepository;
+    }
+
+    public List<Animal> listarAnimais() {
+        return animalRepository.findAll();
+    }
+}
