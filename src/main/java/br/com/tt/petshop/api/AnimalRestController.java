5e1bdaba2f49ce806b4dcb129a2030fb423a61e2
Anderson Martins Silva <anderson_martins@sicredi.com.br>
2020-06-11 21:35:42 -0300

    metodo busca animnal por id

--- src/main/java/br/com/tt/petshop/api/AnimalRestController.java
+++ src/main/java/br/com/tt/petshop/api/AnimalRestController.java
@@ -4,6 +4,7 @@ import br.com.tt.petshop.model.Animal;
 import br.com.tt.petshop.service.AnimalService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,10 @@ public class AnimalRestController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Animal> lista() {
         return animalService.listarAnimais();
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Animal buscarPorId(@PathVariable("id") Long id) {
+        return animalService.buscarPorId(id);
     }
 }
