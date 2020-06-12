370028e9e76415fcb241c78f3908f59d54b61256
Anderson Martins Silva <anderson_martins@sicredi.com.br>
2020-06-11 21:17:57 -0300

    criacao controller para animal

---
+++ src/main/java/br/com/tt/petshop/api/AnimalRestController.java
@@ -0,0 +1,29 @@
+package br.com.tt.petshop.api;
+
+import br.com.tt.petshop.model.Animal;
+import br.com.tt.petshop.service.AnimalService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/animais")
+public class AnimalRestController {
+
+    //Atributo
+    private final AnimalService animalService;
+
+    //Construtor
+    public AnimalRestController(AnimalService animalService) {
+
+        this.animalService = animalService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Animal> lista() {
+        return animalService.listarAnimais();
+    }
+}
