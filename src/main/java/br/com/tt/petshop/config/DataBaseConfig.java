bccc7683c4ece08e16f51ef7206cc0112de70432
Anderson Martins Silva <anderson_martins@sicredi.com.br>
2020-06-14 23:10:57 -0300

    inclusao da dependencia cloud, arquivo bootstrap e DataBaseConfig

---
+++ src/main/java/br/com/tt/petshop/config/DataBaseConfig.java
@@ -0,0 +1,39 @@
+package br.com.tt.petshop.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataBaseConfig {
+
+    //Carrega valores do bootstrap
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+
+    //Atribui valores para o DataSourceBuilder
+    @Bean
+    public DataSource getDataSource()
+    {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(driverClassName);
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
+        return dataSourceBuilder.build();
+    }
+
+
+}
