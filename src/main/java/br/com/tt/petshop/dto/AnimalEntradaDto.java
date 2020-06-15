df942d7c28da4f98829b12f176bf56cc6d542e80
Anderson Martins Silva <anderson_martins@sicredi.com.br>
2020-06-15 19:49:05 -0300

    exemplo de querys

---
+++ src/main/java/br/com/tt/petshop/dto/AnimalEntradaDto.java
@@ -0,0 +1,4 @@
+package br.com.tt.petshop.dto;
+
+public class AnimalEntradaDto {
+}
--- src/main/java/br/com/tt/petshop/repository/AnimalRepository.java
+++ src/main/java/br/com/tt/petshop/repository/AnimalRepository.java
@@ -2,9 +2,12 @@ package br.com.tt.petshop.repository;
 
 import br.com.tt.petshop.model.Animal;
 import br.com.tt.petshop.model.Cliente;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +21,16 @@ public interface AnimalRepository extends
 
     Optional<Animal> findById(Long id);
 
+    //Query Methods
     //Optional<Animal> findByNomeAndCliente(String nome, Cliente cliente);
 
+
+
+    //Query JPQL
+    @Query("select a from Animal a where a.dataNascimento = :dataNascimento")
+    List<Animal> buscarPorDataDeNascimento(@Param("dataNascimento") LocalDate dataNascimento);
+
+    //Query Nativa
+    @Query(value = "select A.NOME FROM TB_ANIMAL A order limit 1 by A.ID desc")
+    String buscarUltimoNomeAnimalCadastrado(String nome);
 }
