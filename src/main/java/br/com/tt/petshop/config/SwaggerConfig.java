a17f6d2d3d0ac7b5c28900509b02bcc0a8c1c954
Anderson Martins Silva <anderson_martins@sicredi.com.br>
2020-06-29 20:42:28 -0300

    inclusao swagger

--- src/main/java/br/com/tt/petshop/config/SwaggerConfig.java
+++ src/main/java/br/com/tt/petshop/config/SwaggerConfig.java
@@ -1,6 +1,9 @@
 package br.com.tt.petshop.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -9,6 +12,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+
+//Somente em nao-prod
+@Profile("!prod")
+
+@ConditionalOnExpression("${app.swagger.enable:true}")
 public class SwaggerConfig {
 
     public Docket docketV1() {
@@ -17,7 +25,6 @@ public class SwaggerConfig {
 //                .apis(RequestHandlerSelectors.any())
 //                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
 //                .apis(RequestHandlerSelectors.basePackage(PetshopApplication.class.getPackageName()))
-                //
                 .apis(RequestHandlerSelectors.basePackage("br.com.tt.petshop.api"))
                 .paths(PathSelectors.any())
                 .build();
