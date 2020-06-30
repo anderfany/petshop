c6b02d296c5b04a0fb0719028a598a25f971444c
Anderson Martins Silva <anderson_martins@sicredi.com.br>
2020-06-29 22:15:22 -0300

    ajuste scripts de banco

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,22 +1,36 @@
-CREATE TABLE TB_UNIDADE (
-	id serial NOT NULL,
-	nome varchar(100) NOT NULL,
-	endereco varchar(150) NOT NULL,
-    primary key (id)
+CREATE TABLE if not exists "tb_unidade"(
+    "id" SERIAL NOT NULL,
+    "nome" VARCHAR(100) NOT NULL,
+    "endereco" VARCHAR(150) NOT null,
+    constraint pk_unidade_id primary key (id)
 );
 
-CREATE TABLE TB_CLIENTE (
-    id serial NOT NULL,
-    nro_cpf VARCHAR(14) not null,
-    txt_nome varchar(255),
-    primary key (id)
+CREATE TABLE if not exists "tb_cliente"(
+    "id" SERIAL NOT NULL,
+    "nro_cpf" VARCHAR(14) NOT NULL,
+    "txt_nome" VARCHAR(255),
+    constraint pk_cliente_id primary key (id)
 );
 
-CREATE TABLE TB_ANIMAL (
-    id serial NOT NULL,
-    nome VARCHAR(100) NOT NULL,
-    data_nascimento DATE,
-    tipo VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
-    --, cliente FOREIGN KEY TB_CLIENTE(id)
+CREATE TABLE if not exists "tb_animal"(
+    "id" SERIAL NOT NULL,
+    "data_nascimento" date,
+    "nome" varchar(255),
+    "tipo_animal" varchar(255),
+   "id_cliente" integer,
+   constraint pk_animal_id primary key (id)
 );
+
+CREATE TABLE if not exists "tb_atendimento"(
+    "id" serial not null,
+    "data_hora" timestamp not null,
+    "descricao_servico" varchar(255),
+    "funcionario" varchar(255),
+    "tipo_servico" varchar(255) not null,
+    "valor_total" decimal(19, 2),
+    "id_animal" bigint not null,
+    constraint pk_atendimento_id primary key (id)
+);
+
+alter table "tb_atendimento" add constraint "fk_cliente_animal" foreign key("id_animal") references "tb_animal"("id");
+alter table "tb_animal" add constraint "fk_animal_cliente" foreign key("id_cliente") references "tb_cliente"("id");
